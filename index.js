7da3085bfd73491938a010bb72d1b343cf6fc761
soren beech <soren.beech@gmail.com>
2019-07-26 16:14:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-clear
+
 var = musicians["John Lennon", "Paul McCartney", "Ringo Star", "Geore Harrison"];
 var = instruments["guitar", "bass guitar", "drums", "lead guitar"];
 function theBeatlesPlay(musicians, instruments) {
-  var = array[];
+  var newArr = [];
   for(var i =0; i < musicians.length; i++) {
     newArr.push(musicians[i] + " plays " + instruments[i]);
   }
-  return array;
+  return newArr;
 }
 const facts=["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
