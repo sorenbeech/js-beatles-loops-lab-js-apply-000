a1b01657901c936a67d0aad539ce9f5b64fd68eb
soren beech <soren.beech@gmail.com>
2019-07-26 16:06:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const facts=["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"];
-  
 Function johnLennonFacts(facts){
   var results = [];
   var count = 0;
