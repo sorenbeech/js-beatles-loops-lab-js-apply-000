f576eeaf6817a5ed294260665794df3116aeeee3
soren beech <soren.beech@gmail.com>
2019-07-26 15:59:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ Function theBeatlesPlay(musicians, instruments){
 const facts=["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  "He hated the sound of his own voice"];
 Function johnLennonFacts(facts){
-  
 while 
 }
