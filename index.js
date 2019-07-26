442450a1ec8acec0325fbcf7b4383a5500182f7d
soren beech <soren.beech@gmail.com>
2019-07-26 15:54:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+var = musicians["John Lennon", "Paul McCartney", "Ringo Star", "Geore Harrison"];
+var = instruments["guitar", "bass guitar", "drums", "lead guitar"];
+Function theBeatlesPlay(musicians, instruments){
+  var =empArr[];
+  for(var i =0; i < musicians.length; i++) {
+    newArr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArr;
+}
